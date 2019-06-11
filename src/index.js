d0b12e97e7ac6b77766caaa7d99b5feb50144aac
zhowiny <zhowiny@gmail.com>
2019-06-11 11:06:08 +0800

    fix bug

--- src/index.js
+++ src/index.js
@@ -1,4 +1,18 @@
-import './components/c-button'
-import './components/c-dialog'
-import './components/c-icon'
-import './components/c-mask'
+import button from './components/c-button'
+import dialog from './components/c-dialog'
+import icon from './components/c-icon'
+import mask from './components/c-mask'
+
+const install = function(Vue, opts = {}) {
+}
+
+const components = {
+  version: process.env.VERSION,
+  button,
+  dialog,
+  icon,
+  mask,
+  install,
+}
+
+module.exports.default = module.exports = components
